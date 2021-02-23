4b1bbd5236e65b682fe9b8b391d65b7d18b3a3a2
Xiaodong Yu <xyu@lambda3.cels.anl.gov>
2021-02-23 00:56:28 -0800

    NEW: add autocorr kernel.

--- zc/src/cuZC_typeTwo.cu
+++ zc/src/cuZC_typeTwo.cu
@@ -54,3 +54,67 @@ __global__ void type_two(float *data, float *der, int r3, int r2, int r1, size_t
 
 }
 
+__global__ void auto_corr(float *data, float *autocor, int r3, int r2, int r1, float avg, size_t autosize) 
+{
+    int tidx = threadIdx.x;
+    int tidy = threadIdx.y;
+    int bid = blockIdx.x;
+
+    float base;
+    int i, j;
+    int h=bid*(16-autosize);
+    double dx, dy, dz;
+    
+    int wsize = (r2-autosize)/(16-autosize) + ((r2-autosize)%(16-autosize)?1:0);
+    int lsize = (r1-autosize)/(16-autosize) + ((r1-autosize)%(16-autosize)?1:0);
+    
+    extern __shared__ float shared[];
+    float *bdata = shared;
+    float *cor = &shared[blockDim.x * blockDim.y * 16];
+    unsigned mask;
+
+    for (int w=0; w<wsize*(16-autosize); w+=(16-autosize)){
+        for (int l=0; l<lsize*(16-autosize); l+=(16-autosize)){
+            for (i=0; i<16; i++){
+                if ((h+i)<r3 && (l+tidx)<r1 && (w+tidy)<r2){
+                    bdata[i*16*16+tidy*16+tidx] = data[(h+i)*r1*r2+(w+tidy)*r1+(l+tidx)];
+                }
+                    
+            }
+            __syncthreads();                  
+
+            for (i=0; i<(16-autosize); i++){
+                if (tidx<(16-autosize) && tidy<(16-autosize)){
+                    if ((h+i)<(r3-autosize) && (l+tidx)<(r1-autosize) && (w+tidy)<(r2-autosize)){
+                        mask = __activemask();
+                        base = bdata[i*16*16+tidy*16+tidx];
+                    
+                        for (j=1; j<=autosize; j++){
+                            sum = (bdata[(i+j)*16*16+(tidy+j)*16+tidx+j]-avg) * (base-avg);
+
+                            for (int offset = warpSize/2; offset > 0; offset /= 2) 
+                            sum += __shfl_down_sync(mask, sum, offset);
+
+                            if (tidx==0) cor[blockDim.y*(j-1)+tidy] = sum;
+                        }
+                    }
+                }
+                __syncthreads(); 
+
+                if (tidy<autosize){
+                    if (tidx < (16-autosize) && (w+tidx)<(r2-autosize))
+                    {
+                        sum = cor[blockDim.y*tidy+tidx];
+                        mask = __activemask();
+                    } else sum = 0;
+
+                    for (int offset = warpSize/2; offset > 0; offset /= 2) 
+                        sum += __shfl_down_sync(mask, sum, offset);
+
+                    if (tidx==0) autocor[gridDim.x*tidy+blockIdx.x] += sum;
+                }
+                __syncthreads();                  
+            }
+        }
+    }
+}
